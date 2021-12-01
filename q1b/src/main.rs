70ace835f977b14b488ba77d3f6b764dd24137d5
Marco Caselli <marco.caselli@mindfoundry.ai>
2021-12-01 10:02:19 +0000

    Add solutions for day 1

--- q1b/src/main.rs
+++ q1b/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let items: Vec<usize> = include_str!("../input.txt")
+        .lines()
+        .map(|i| i.parse().unwrap())
+        .collect();
+    let mut counter:usize = 0;
+    let mut previous = usize::MAX;
+    for w in items.windows(3) {
+        let this = w.iter().sum();
+        if this > previous {
+            counter += 1;
+        }
+        previous = this;
+    }
+    println!("{}",counter);
 }
