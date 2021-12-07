9c50cc14c5d168c825c2cce7940c88f8f82b788d
Marco Caselli <marco.caselli@mindfoundry.ai>
2021-12-07 09:58:59 +0000

    Refine

--- q7b/src/main.rs
+++ q7b/src/main.rs
@@ -5,10 +5,10 @@ pub fn main() {
         .map(|i| i.parse().unwrap())
         .collect();
     let len = crabs.len() as f32;
-    let mean:f32 = crabs.iter().sum::<f32>()/len;
-    let mean_index:f32 = crabs.iter().map(|c| (c<&mean) as i32).sum::<i32>() as f32;
-    let target = (mean + (len - mean_index*2.0)/(2.0*len)) as i32;
-    let fuel:i32 = crabs.iter().map(|c| (*c as i32 -target).abs()).map(|d| d*(d+1)/2).sum();
+    let mean = crabs.iter().sum::<f32>()/len;
+    let mean_index = crabs.iter().map(|c| (c<&mean) as i32).sum::<i32>() as f32;
+    let target = (mean + (len - mean_index*2.0)/(2.0*len)).round();
+    let fuel:i32 = crabs.iter().map(|c| (c-target).abs() as i32).map(|d| d*(d+1)/2).sum();
     println!("{:?}", fuel );
 }
 
