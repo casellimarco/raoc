ce495e7e430aaf4c49ac1f65ecd36a2a2681a25f
Marco Caselli <marco.caselli@mindfoundry.ai>
2021-12-07 09:34:39 +0000

    Make it correct but uglier

--- q7b/src/main.rs
+++ q7b/src/main.rs
@@ -1,11 +1,14 @@
 pub fn main() {
-    let crabs:Vec<i32> = include_str!("../input.txt")
+    let crabs:Vec<f32> = include_str!("../input.txt")
         .trim()
         .split(",")
         .map(|i| i.parse().unwrap())
         .collect();
-    let mean:i32 = crabs.iter().sum::<i32>()/crabs.len() as i32;
-    let fuel:i32 = crabs.iter().map(|c| (c-mean).abs()).map(|d| d*(d+1)/2).sum();
+    let len = crabs.len() as f32;
+    let mean:f32 = crabs.iter().sum::<f32>()/len;
+    let mean_index:f32 = crabs.iter().map(|c| (c<&mean) as i32).sum::<i32>() as f32;
+    let target = (mean + (len - mean_index*2.0)/(2.0*len)) as i32;
+    let fuel:i32 = crabs.iter().map(|c| (*c as i32 -target).abs()).map(|d| d*(d+1)/2).sum();
     println!("{:?}", fuel );
 }
 
