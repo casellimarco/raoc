d1cd3734702fd950d3e44c186f1cc127c4386330
Marco Caselli <casellimarco7@gmail.com>
2021-12-08 21:29:57 +0000

    Solve day8b

--- q8b/src/main.rs
+++ q8b/src/main.rs
@@ -5,28 +5,38 @@ pub fn main() {
     let mut counter:usize = 0;
     let cha = vec!['a','b','c','d','e','f','g'];
     let ctd:HashMap<&char, usize> = HashMap::from_iter(cha.iter().enumerate().map(|(d,c)| (c,d)));
-    let sizes:Vec<usize> = vec![2,3,4,7];
+    let digits:HashMap<&str, usize> = HashMap::from([
+        ("abcefg",0), ("cf",1), ("acdeg",2), ("acdfg",3),("bcdf",4),
+        ("abdfg",5), ("abdefg",6), ("acf", 7), ("abcdefg",8), ("abcdfg", 9)]);
     for l in include_str!("../input.txt").lines(){
         let v:Vec<&str> = l.split(" | ").collect();
-        let mut all:Vec<&str> = v[0].split_whitespace().collect();
+        let mut inputs:Vec<&str> = v[0].split_whitespace().collect();
         let outputs:Vec<&str> = v[1].split_whitespace().collect();
-        all.extend(&outputs);
-        all.sort_by(|a,b| a.len().cmp(&b.len()));
+        inputs.sort_by(|a,b| a.len().cmp(&b.len()));
+        let mut cha_map:Vec<&char> = Vec::new();
         for dtc in cha.iter().permutations(cha.len()){
-            for entry in &all{
+            let mut pass:usize = 0;
+            for entry in &inputs{
                  let mut mapped_entry = entry.chars().map(|c| dtc[ctd[&c]]).collect::<Vec<&char>>();
                  mapped_entry.sort();
                  let mapped_string: String = mapped_entry.into_iter().collect();
-                 println!("{:?}", mapped_string);
-                 // let mapped_entry = entry.chars().map(|c| ctd[dtc[&c]]).collect().sort();
+                 if digits.get(&mapped_string.as_str()).is_none(){
+                     break;
+                 }else{
+                     pass += 1;
+                 }
             }
-            // println!("{:?}", dtc);
+            if pass == 10{
+                cha_map = dtc;
+                break;
+            }
         }
-        // println!("{:?}", all);
-        for output in v[1].split_whitespace(){
-            if sizes.contains(&output.len()) {
-                counter+=1;
-            }
+        for (i, entry) in outputs.iter().enumerate(){
+             let mut mapped_entry = entry.chars().map(|c| cha_map[ctd[&c]]).collect::<Vec<&char>>();
+             mapped_entry.sort();
+             let mapped_string: String = mapped_entry.into_iter().collect();
+             let p = (outputs.len() - i - 1) as u32;
+             counter += digits.get(&mapped_string.as_str()).unwrap()*usize::pow(10,p);
         }
     }
     println!("{:?}", counter);
