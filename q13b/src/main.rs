39a4870dc3f799d4575e3e5e2004503233568744
Marco Caselli <marco.caselli@mindfoundry.ai>
2021-12-13 14:53:19 +0000

    Small refactor

--- q13b/src/main.rs
+++ q13b/src/main.rs
@@ -2,6 +2,7 @@ use std::collections::HashSet;
 pub fn main() {
     let mut coordinates_done = false;
     let mut points = HashSet::new();
+    let mut max = vec![0, 0];
     let mut folds:Vec<Vec<usize>> = Vec::new();
     for line in include_str!("../input.txt").lines(){
         if line.is_empty() {
@@ -12,30 +13,29 @@ pub fn main() {
             points.insert(line.split(",").map(|x| x.parse().unwrap()).collect::<Vec<usize>>()[..2].to_vec());
         }else{
             let trimmed = line.replace("fold along ", "").replace("x", "0").replace("y", "1");
-            folds.push(trimmed.split("=").map(|x| x.parse().unwrap()).collect());
+            let fold:Vec<usize> = trimmed.split("=").map(|x| x.parse().unwrap()).collect();
+            max[fold[0]] = fold[1];
+            folds.push(fold);
         }
     }
-    let mut max = vec![0, 0];
-    for fold in folds{
-        let axis = fold[0];
-        let k = fold[1];
-        max[axis] = k;
-        let mut new_points = HashSet::new();
-        for mut p in points{
+    let mut new_points = HashSet::new();
+    for mut p in points{
+        for fold in &folds{
+            let axis = fold[0];
+            let k = fold[1];
             if p[axis] > k{
                 p[axis] = 2*k - p[axis];
             }
-            new_points.insert(p);
         }
-        points = new_points;
+        new_points.insert(p);
     }
     for i in 0..max[1]{
         let mut row = "".to_string();
         for j in 0..max[0]{
-            if points.contains(&vec![j, i]){
-                row.push_str(&"#");
+            if new_points.contains(&vec![j, i]){
+                row.push_str(&"■");
             }else{
-                row.push_str(&".");
+                row.push_str(&" ");
             }
         }
         println!("{:?}", row);
