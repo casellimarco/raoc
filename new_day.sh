89c4d1c6fd236b664d29f62a9d64b7185dd61864
Marco Caselli <marco.caselli@mindfoundry.ai>
2021-12-13 21:56:52 +0000

    Better new_day.sh

--- new_day.sh
+++ new_day.sh
@@ -12,3 +12,10 @@ cargo new q${DAY}b
 
 curl -b session=${AOC_SESSION} $URL/input > q${DAY}a/input.txt
 cp q${DAY}a/input.txt q${DAY}b/input.txt
+
+for f in q${DAY}a q${DAY}b; do
+    target=$f/Cargo.toml
+    echo "" >> $target
+    echo "[lib]" >> $target
+    echo "path = [\"src/main.rs\"]" >> $target
+done
