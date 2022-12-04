5c734f17ca7309fd1d9e2447aacadae0075151ac
Marco Caselli <marco.caselli@mindfoundry.ai>
2022-12-04 11:15:33 +0100

    Open in vscode the rs files

--- new_day.sh
+++ new_day.sh
@@ -4,14 +4,17 @@ source ~/.secrets
 DAY=${1:-$(date +'%-d')}
 YEAR=${2:-$(date +'%Y')}
 
-URL=https://adventofcode.com/${YEAR}/day/${DAY}
-browse $URL
-
 mkdir -p ${YEAR}
 PPATH=${YEAR}/q${DAY}
 
 cargo new ${PPATH}a
 cargo new ${PPATH}b
+
+code ${PPATH}a/src/main.rs ${PPATH}b/src/main.rs
+sleep 1
+
+URL=https://adventofcode.com/${YEAR}/day/${DAY}
+browse $URL
 
 curl -b session=${AOC_SESSION} $URL/input > ${PPATH}a/input.txt
 cp ${PPATH}a/input.txt ${PPATH}b/input.txt
