00755d689d0e4da20aa0898fbc1abc4ce5a2e83c
casellimarco7 <marco.caselli@mindfoundry.ai>
2023-12-01 13:33:32 +0100

    Update new day to use python

--- new_day.sh
+++ new_day.sh
@@ -1,21 +1,33 @@
 #!/bin/bash
 source ~/.secrets
 
-DAY=${1:-$(date +'%-d')}
-YEAR=${2:-$(date +'%Y')}
+LANGUAGE=${1:-python}
+DAY=${2:-$(date +'%-d')}
+YEAR=${3:-$(date +'%Y')}
 
 mkdir -p ${YEAR}
 PPATH=${YEAR}/q${DAY}
 
-cargo new ${PPATH}a
-cargo new ${PPATH}b
-
-code ${PPATH}a/src/main.rs ${PPATH}b/src/main.rs
+# if LANGUAGE is rust then
+if [ $LANGUAGE = "rust" ]; then
+    cargo new ${PPATH}a
+    cargo new ${PPATH}b
+    code ${PPATH}a/src/main.rs ${PPATH}b/src/main.rs
+else
+    mkdir -p ${PPATH}
+    touch ${PPATH}/main.py
+    code ${PPATH}/main.py
+fi
 # gives time to vsc process to complete in order to have as end state the browser in foreground 
 sleep 1
 
 URL=https://adventofcode.com/${YEAR}/day/${DAY}
 browse $URL
 
-curl -b session=${AOC_SESSION} $URL/input > ${PPATH}a/input.txt
-cp ${PPATH}a/input.txt ${PPATH}b/input.txt
+
+if [ $LANGUAGE = "rust" ]; then
+    curl -b session=${AOC_SESSION} $URL/input > ${PPATH}a/input.txt
+    cp ${PPATH}a/input.txt ${PPATH}b/input.txt
+else
+    curl -b session=${AOC_SESSION} $URL/input > ${PPATH}/input.txt
+fi
