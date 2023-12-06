4d6d22fe87d02f94aebad5b3c0d03b1221f81a7e
casellimarco7 <marco.caselli@mindfoundry.ai>
2023-12-06 12:03:29 +0100

    Small refactor

--- 2023/6/main.py
+++ 2023/6/main.py
@@ -5,22 +5,17 @@ time_str, distance_str = data.split("\n")
 time = list(map(int, time_str.split()[1:]))
 distance = list(map(int, distance_str.split()[1:]))
 
-
 # w * (t - w) > d
 # -w**2 + t*w - d > 0
 # delta = t**2 - 4*d
-
-tot = 1
-for t, d in zip(time, distance):
+def num_solutions(t, d):
     delta = np.sqrt(t**2 - 4*(d+1))
     min_w = np.ceil((-t -delta)/2)  
     max_w = np.floor((-t +delta)/2)
-    tot *= int(max_w - min_w + 1)
-print(1, tot)
+    return int(max_w - min_w + 1)
+
+print(1, np.prod([num_solutions(t,d) for t,d in zip(time, distance)]))
 
 t = int("".join(map(str, time)))
 d = int("".join(map(str, distance)))
-delta = np.sqrt(t**2 - 4*(d+1))
-min_w = np.ceil((-t -delta)/2)  
-max_w = np.floor((-t +delta)/2)
-print(2, int(max_w - min_w + 1))
+print(2, num_solutions(t,d))
