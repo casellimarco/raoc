70a77484322bda834dd047d0f1e92d97f372e35a
casellimarco7 <marco.caselli@mindfoundry.ai>
2024-12-11 17:06:13 +0100

    Save time and one line

--- 2024/11/main.py
+++ 2024/11/main.py
@@ -6,12 +6,11 @@ stones = list(map(int, data.split()))
 @lru_cache(maxsize=None)
 def convert(stone):
     if stone == 0:
-        return [1]
+        return 1,
     elif (digits:=len(str_stone:=str(stone))) % 2 == 0:
         half = digits // 2
-        return [int(str_stone[:half]), int(str_stone[half:])]
-    else:
-        return [stone * 2024]
+        return int(str_stone[:half]), int(str_stone[half:])
+    return stone * 2024,
 
 @lru_cache(maxsize=None)
 def process(stone, step):
