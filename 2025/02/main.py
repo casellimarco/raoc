9749efa9813185ba3f9040ce3c816374bf10d721
casellimarco <casellimarco7@gmail.com>
2025-12-03 05:34:53 +0000

    Solve 25/02b

--- 2025/02/main.py
+++ 2025/02/main.py
@@ -1,28 +1,50 @@
-
-data = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"
-
-
 from aocd import data
 from math import ceil
 
+
+def prime_factors(num):
+  factors = []
+  factor = 2
+  while (num >= 2):
+    if (num % factor == 0):
+      factors.append(factor)
+      num = num / factor
+    else:
+      factor += 1
+  return list(set(factors))
+
+
+def get_value(d, p, a, b):
+    factor = sum([10**(d//p*i) for i in range(p)])
+    upper = 10**d//factor
+    lower = 10**(d-1)//factor
+    if d == len(b):
+        upper = int(b) // factor 
+    if d == len(a):
+        lower = ceil((int(a))/factor) - 1
+
+    return factor*(sum_n(upper) - sum_n(lower))
+
+
+def sum_n(n): 
+    return (n*(n+1))//2
+
+
 part1=0
-
-sum_n = lambda n: (n*(n+1))//2
-
+part2=0
 for interval in data.split(","):
     a,b = interval.split("-")
     for d in range(len(a),len(b)+1):
-        if d%2 == 1:
-            continue
-        factor = 10**(d//2) + 1
-        upper = factor - 2
-        lower = 10**(d-1)//factor
-        if d == len(b):
-            upper = int(b) // factor 
-        if d == len(a):
-            lower = ceil((int(a))/factor) - 1
+        primes = prime_factors(d)
+        for i, p in enumerate(primes):
+            value = get_value(d, p, a, b);
+            part2+=value
+            if p == 2:
+                part1+=value
+            for q in primes[i+1:]:
+                part2-= get_value(d, p*q, a, b)
 
-        part1+=factor*(sum_n(upper) - sum_n(lower))
 
 print(part1)
+print(part2)
 
