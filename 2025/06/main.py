9c97ea7f1900338b970533e3fedd2e7f267b9c97
casellimarco <casellimarco7@gmail.com>
2025-12-06 17:11:44 +0100

    Solve 2025/06b

--- 2025/06/main.py
+++ 2025/06/main.py
@@ -12,4 +12,22 @@ mask = np.array(a[-1]) == "+"
 
 print(s[mask].sum() + p[~mask].sum())
 
+a = np.array([list(d) for d in data.splitlines()][:-1]).T
+n = ["".join(list(d)).split() for d in a] + [[]]
+
+part2 = 0
+i = 0
+c = []
+for d in n:
+    if not d:
+        if mask[i]:
+            part2+=np.sum(c)
+        else:
+            part2+=np.prod(c)
+        i += 1
+        c = []
+    else:
+        c.append(int(d[0]))
+
+print(part2)
 
