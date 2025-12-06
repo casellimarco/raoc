603e9d09276b9df20209bd231de0c15934bf8ba8
casellimarco <casellimarco7@gmail.com>
2025-12-06 18:55:16 +0100

    Solve 2025/05b

--- 2025/05/main.py
+++ 2025/05/main.py
@@ -1,21 +1,3 @@
-
-
-data="""3-5
-10-14
-16-20
-12-18
-21-24
-29-32
-23-30
-19-22
-
-
-1
-5
-8
-11
-17
-32"""
 from aocd import data
 
 fresh = set()
@@ -39,36 +21,24 @@ for i in ids:
 
 print(part1)
 
+a = []
+for s,e in fresh:
+    a.extend([(s,0), (e,1)])
 
-def union_pairs(fresh):
-    new = set()
-    checked = set()
-    for i, f in enumerate(fresh):
-        for j, g in enumerate(fresh):
-            if i in checked:
-                break
-            if j <= i or j in checked:
-                continue
-            if f[0] <= g[0] <= f[1] or f[0] <= g[1] <= f[1]:
-                new.add((min(f[0],g[0]), max(f[1],g[1])))
-                checked.add(i)
-                checked.add(j)
-                break
-        if not i in checked:
-            new.add(f)
-            checked.add(i)
+a.sort(key = lambda p: p[0] + 0.1*p[1])
 
-    return new                           
-
-old = set()
-
-while old != fresh:
-    old = fresh.copy()
-    fresh = union_pairs(fresh)
-
-
+o = 0
+s = None
 part2 = 0
-for f in fresh:
-    part2+=f[1] - f[0] + 1
+for b,t in a:
+    if t:
+        o -= 1
+        if o == 0:
+            part2+= b - s + 1
+    else:
+        o += 1
+        if o == 1:
+            s = b
 
 print(part2)
+
