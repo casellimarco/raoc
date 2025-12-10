959f19d5886cc8e4296ea25e7cb5014bcb9b15e3
casellimarco7 <marco.caselli@mindfoundry.ai>
2025-12-10 21:38:30 +0100

    Solve 2025/09b

--- 2025/09/main.py
+++ 2025/09/main.py
@@ -1,37 +1,18 @@
 from aocd import data
-import numpy as np
-from itertools import batched
+from shapely import Polygon, box
 
 r = [list(map(int, p.split(","))) for p in data.splitlines()]
-r = np.array(r)
-a = []
+part1 = 0
+part2 = 0
+polygon = Polygon(r)
 for i, c in enumerate(r):
     for d in r[i+1:]:
-        a.append(np.prod(np.abs(c -d)+1))
-
-print(np.max(a))
-
-m = np.zeros(np.max(r, axis=0)+1, dtype=np.bool)
-for i, (c0, c1) in enumerate(r):
-    d0, d1 = r[i-1]
-    m[min(c0, d0):max(c0,d0)+1,min(c1,d1):max(c1,d1)+1]==True
-
-for i, l in enumerate(m):
-    print(i, len(m))
-    for c, d in batched(np.argwhere(m), n=2):
-        if d == c+1:
-            continue
-        f[l][c:d+1] = True
-
-a = []
-for i, (c0, c1) in enumerate(r):
-    for (d0, d1) in r[i+1:]:
-        if np.all(f[min(c0, d0):max(c0,d0)+1,min(c1,d1):max(c1,d1)+1]):
-            a.append(np.prod(np.abs(c -d)+1))
-
-print(np.max(a))
-
-
-
-
-
+        c0,c1 = c
+        d0,d1 = d
+        area = (abs(c0-d0)+1)*(abs(c1-d1)+1)
+        part1 = max(part1, area)
+        b = box(min(c0, d0), min(c1,d1), max(c0,d0),max(c1,d1))
+        if polygon.contains(b):
+            part2 = max(part2, area)
+print(part1)
+print(part2)
