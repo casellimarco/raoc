abf336efb11b8650a54f7ffb02f84f2451e80ad6
casellimarco7 <marco.caselli@mindfoundry.ai>
2025-12-11 11:48:39 +0100

    Simplify script

--- 2025/09/main.py
+++ 2025/09/main.py
@@ -5,10 +5,8 @@ r = [list(map(int, p.split(","))) for p in data.splitlines()]
 part1 = 0
 part2 = 0
 polygon = Polygon(r)
-for i, c in enumerate(r):
-    for d in r[i+1:]:
-        c0,c1 = c
-        d0,d1 = d
+for i, (c0,c1) in enumerate(r):
+    for d0,d1 in r[i+1:]:
         area = (abs(c0-d0)+1)*(abs(c1-d1)+1)
         part1 = max(part1, area)
         b = box(min(c0, d0), min(c1,d1), max(c0,d0),max(c1,d1))
